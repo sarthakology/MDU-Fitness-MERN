d098a9d4a01a7d5012b8d4ba47fce701cdf457c5
Sarthak <109920827+sarthakology@users.noreply.github.com>
2024-08-21 15:48:01 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -15,81 +15,100 @@ import StrengthTrainingPage from './components/Pages/StrengthTrainingPage.js';
 import FlexibilityMobilityPage from './components/Pages/FlexibilityMobilityPage.js';
 import CrossFitWorkoutsPage from './components/Pages/CrossFitWorkoutsPage.js';
 import HomeWorkoutsPage from './components/Pages/HomeWorkoutsPage.js';
-import Enquiry from './components/EnquiryPage/Enquiry.js'
+import Enquiry from './components/EnquiryPage/Enquiry.js';
 
-import FrontSlidingAdvertisement from './components/FrontSlidingAdvertisement.js'
+import FrontSlidingAdvertisement from './components/FrontSlidingAdvertisement.js';
 import ProgramPage from './components/Pages/ProgramPage.js';
 import Footer from './components/Footer.js';
 
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route, 
-  } 
-  from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from './components/HomePage/HomePage.js';
 
 function App() {
   return (
     <div>
       <Router>
-        
         <Routes>
-            <Route path="/" element={<> <SigninPage/></>}/>
-        </Routes>
-        <Routes>
-            <Route path="/home" element={<> 
-            <Navbar/>
-            <HomePage/>
-            <FrontSlidingAdvertisement/>
-            <ProgramPage/>
-            
-            </>}/>
-        </Routes>
-        <Routes>
-            <Route path="/register" element={<RegisterPage/>}/>
-        </Routes> 
-        <Routes>
-            <Route path="/BulkingDietPage" element={<><Navbar/><BulkingDietPage/></>}/>
-          </Routes> 
-           <Routes>
-            <Route path="/CuttingDietPage" element={<><Navbar/><CuttingDietPage/></>}/>
-          </Routes> 
-          <Routes>
-            <Route path="/LeanBulkDietPage" element={<><Navbar/><LeanBulkDietPage/></>}/>
-          </Routes>
-          <Routes>
-            <Route path="/BMICalculator" element={<><Navbar/><BMICalculator/></>}/>
-          </Routes>
-          <Routes>
-            <Route path="/NutritionTracker" element={<><Navbar/><NutritionTracker/></>}/>
-          </Routes>
-          <Routes>
-            <Route path="/WorkoutTracker" element={<><Navbar/><WorkoutTracker/></>}/>
-          </Routes> 
-          <Routes>
-            <Route path="/SupplementPage" element={<><Navbar/><SupplementPage/></>}/>
-          </Routes> 
+          {/* Authentication Routes */}
+          <Route path="/" element={<SigninPage />} />
+          <Route path="/register" element={<RegisterPage />} />
 
-          <Routes>
-            <Route path="/MotivationQuotes" element={<><Navbar/><MotivationQuotes/></>}/>
-          </Routes> 
-          <Routes>
-            <Route path="/StrengthTrainingPage" element={<><Navbar/><StrengthTrainingPage/></>}/>
-          </Routes>
-          <Routes>
-            <Route path="/FlexibilityMobilityPage" element={<><Navbar/><FlexibilityMobilityPage/></>}/>
-          </Routes>
-          <Routes>
-            <Route path="/CrossFitWorkoutsPage" element={<><Navbar/><CrossFitWorkoutsPage/></>}/>
-          </Routes>
-          <Routes>
-            <Route path="/HomeWorkoutsPage" element={<><Navbar/><HomeWorkoutsPage/></>}/>
-          </Routes>
-          <Routes>
-            <Route path="/enquiry" element={<><Navbar/><Enquiry/></>}/>
-          </Routes>
-          <Footer/>
+          {/* Home Page */}
+          <Route
+            path="/home"
+            element={
+              <>
+                <Navbar />
+                <HomePage />
+                <FrontSlidingAdvertisement />
+                <ProgramPage />
+              </>
+            }
+          />
+
+          {/* Diet Pages */}
+          <Route
+            path="/BulkingDietPage"
+            element={<><Navbar /><BulkingDietPage /></>}
+          />
+          <Route
+            path="/CuttingDietPage"
+            element={<><Navbar /><CuttingDietPage /></>}
+          />
+          <Route
+            path="/LeanBulkDietPage"
+            element={<><Navbar /><LeanBulkDietPage /></>}
+          />
+
+          {/* Fitness Tools Pages */}
+          <Route
+            path="/BMICalculator"
+            element={<><Navbar /><BMICalculator /></>}
+          />
+          <Route
+            path="/NutritionTracker"
+            element={<><Navbar /><NutritionTracker /></>}
+          />
+          <Route
+            path="/WorkoutTracker"
+            element={<><Navbar /><WorkoutTracker /></>}
+          />
+          <Route
+            path="/SupplementPage"
+            element={<><Navbar /><SupplementPage /></>}
+          />
+
+          {/* Miscellaneous Pages */}
+          <Route
+            path="/MotivationQuotes"
+            element={<><Navbar /><MotivationQuotes /></>}
+          />
+          <Route
+            path="/StrengthTrainingPage"
+            element={<><Navbar /><StrengthTrainingPage /></>}
+          />
+          <Route
+            path="/FlexibilityMobilityPage"
+            element={<><Navbar /><FlexibilityMobilityPage /></>}
+          />
+          <Route
+            path="/CrossFitWorkoutsPage"
+            element={<><Navbar /><CrossFitWorkoutsPage /></>}
+          />
+          <Route
+            path="/HomeWorkoutsPage"
+            element={<><Navbar /><HomeWorkoutsPage /></>}
+          />
+
+          {/* Enquiry Page */}
+          <Route
+            path="/enquiry"
+            element={<><Navbar /><Enquiry /></>}
+          />
+        </Routes>
+
+        {/* Footer */}
+        <Footer />
       </Router>
     </div>
   );
